7046bba528df9edf03d1ff47ceae9f66f31c0f65
Vision.Mac <chao2.ma@changhong.com>
2020-10-27 22:39:40 +0800

    解决spring-boot-admin不能访问swagger的问题

---
+++ sei-manager-service/src/main/java/com/changhong/sei/manager/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.changhong.sei.manager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 实现功能：解决spring-boot-admin不能访问swagger的问题
+ *
+ * @author 马超(Vision.Mac)
+ * @version 1.0.00  2020-10-27 22:32
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/templates/**.js").addResourceLocations("classpath:/templates/");
+        registry.addResourceHandler("/templates/**.css").addResourceLocations("classpath:/templates/");
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
