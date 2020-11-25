98538398c01a6510cd55275ef516ab016bf6370c
Vision.Mac <chao2.ma@changhong.com>
2020-11-25 16:54:06 +0800

    开发运维平台集成

--- sei-manager-service/src/main/java/com/changhong/sei/deploy/service/DeployTemplateStageService.java
+++ sei-manager-service/src/main/java/com/changhong/sei/deploy/service/DeployTemplateStageService.java
@@ -1,11 +1,9 @@
 package com.changhong.sei.deploy.service;
 
 import com.changhong.sei.core.dao.BaseRelationDao;
-import com.changhong.sei.core.dto.ResultData;
 import com.changhong.sei.core.service.BaseRelationService;
 import com.changhong.sei.core.service.bo.OperateResultWithData;
 import com.changhong.sei.deploy.dao.DeployTemplateStageDao;
-import com.changhong.sei.deploy.dto.DeployTemplateStageResponse;
 import com.changhong.sei.deploy.entity.DeployStage;
 import com.changhong.sei.deploy.entity.DeployTemplate;
 import com.changhong.sei.deploy.entity.DeployTemplateStage;
@@ -13,6 +11,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
@@ -64,4 +63,24 @@ public class DeployTemplateStageService extends BaseRelationService<DeployTempla
         return OperateResultWithData.operationFailure("修改的模板阶段不存在.");
     }
 
+    /**
+     * 批量数据保存操作 其实现只是简单循环集合每个元素调用
+     * 因此并无实际的Batch批量处理，如果需要数据库底层批量支持请自行实现
+     *
+     * @param entities 待批量操作数据集合
+     */
+    @Override
+    public void save(Collection<DeployTemplateStage> entities) {
+        DeployStage stage;
+        for (DeployTemplateStage templateStage : entities) {
+            if (Objects.isNull(templateStage)) {
+                continue;
+            }
+            stage = templateStage.getChild();
+            if (Objects.nonNull(stage)) {
+                templateStage.setPlayscript(stage.getPlayscript());
+            }
+        }
+        super.save(entities);
+    }
 }
