74c3d3285ebe1945849a271de71b5d5fe7c589de
Vision.Mac <chao2.ma@changhong.com>
2021-08-24 12:43:50 +0800

    删除@Lob注解

--- sei-manager-service/src/main/java/com/changhong/sei/manager/entity/Menu.java
+++ sei-manager-service/src/main/java/com/changhong/sei/manager/entity/Menu.java
@@ -80,7 +80,6 @@ public class Menu extends BaseAuditableEntity implements TreeEntity<Menu> {
     /**
      * 图标文件数据
      */
-    @Lob
     @Basic(fetch = FetchType.LAZY)
     @Column(name = "icon_file_data")
     private String iconFileData;
