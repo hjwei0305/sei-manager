318e4c6f101532baefabadca165ce572627b36c5
Vision.Mac <chao2.ma@changhong.com>
2022-02-24 16:09:57 +0800

    增加外网支持

--- sei-manager-service/src/main/java/com/changhong/sei/common/TokenInterceptor.java
+++ sei-manager-service/src/main/java/com/changhong/sei/common/TokenInterceptor.java
@@ -22,8 +22,8 @@ public class TokenInterceptor implements ClientHttpRequestInterceptor {
         //获取令牌
         String token = ContextUtil.getToken();
         //将令牌放入请求header中
-        request.getHeaders().add("x-sid", "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjaGFvMi5tYSIsImxvZ2luQWNjb3VudCI6ImNoYW8yLm1hIiwiaXAiOiJVbmtub3duIiwidXNlck5hbWUiOiLpqazotoUiLCJsb2NhbGUiOiJ6aF9DTiIsInVzZXJJZCI6IkRDQTE2MzRGLTQ0MTgtMTFFQi05QjZCLTAyNDJDMEE4NDMxRiIsInJhbmRvbUtleSI6Ijk2RTBEMzUwLTk1MEUtMTFFQy1BQUFGLTAyNDJDMEE4NDMwNSIsImF1dGhvcml0eVBvbGljeSI6Ikdsb2JhbEFkbWluIiwidXNlclR5cGUiOiJFbXBsb3llZSIsImV4cCI6MTY0NTc1MTQ0MywiaWF0IjoxNjQ1NjY1MDQzLCJ0ZW5hbnQiOiJTRUkiLCJhY2NvdW50IjoiY2hhbzIubWEifQ.IUKaAVOm8yWPn-H0y-K4Y7b4ce6_Vo9aVXkmu5lhq8ouo2SGSFSbu9dCuClcc4Z5eU8uj86qrxeORtBuRCKtYw");
-        // request.getHeaders().add("x-sid", token);
+        // request.getHeaders().add("x-sid", "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjaGFvMi5tYSIsImxvZ2luQWNjb3VudCI6ImNoYW8yLm1hIiwiaXAiOiJVbmtub3duIiwidXNlck5hbWUiOiLpqazotoUiLCJsb2NhbGUiOiJ6aF9DTiIsInVzZXJJZCI6IkRDQTE2MzRGLTQ0MTgtMTFFQi05QjZCLTAyNDJDMEE4NDMxRiIsInJhbmRvbUtleSI6Ijk2RTBEMzUwLTk1MEUtMTFFQy1BQUFGLTAyNDJDMEE4NDMwNSIsImF1dGhvcml0eVBvbGljeSI6Ikdsb2JhbEFkbWluIiwidXNlclR5cGUiOiJFbXBsb3llZSIsImV4cCI6MTY0NTc1MTQ0MywiaWF0IjoxNjQ1NjY1MDQzLCJ0ZW5hbnQiOiJTRUkiLCJhY2NvdW50IjoiY2hhbzIubWEifQ.IUKaAVOm8yWPn-H0y-K4Y7b4ce6_Vo9aVXkmu5lhq8ouo2SGSFSbu9dCuClcc4Z5eU8uj86qrxeORtBuRCKtYw");
+        request.getHeaders().add("x-sid", token);
         return execution.execute(request, body);
     }
 }
--- sei-manager-service/src/main/java/com/changhong/sei/log/controller/LogController.java
+++ sei-manager-service/src/main/java/com/changhong/sei/log/controller/LogController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
